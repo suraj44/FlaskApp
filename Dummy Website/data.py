7a1707707d913f196ac83635d266db325798f970
suraj44 <suraj1998@gmail.com>
2018-01-04 15:26:20 +0530

    first commit

---
+++ Dummy Website/data.py
@@ -0,0 +1,27 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'name': 'Article One',
+			'body': 'Pish poshhhhhhhhhhhhhhhhhhhhh',
+			'author': 'Suraj Singh',
+			'create_date': '04-01-2017'
+		},
+		{
+			'id': 2,
+			'name': 'Article Two',
+			'body': 'Pish poshhhhhhhhhhhhhhhhhhhhh',
+			'author': 'Michael Westen',
+			'create_date': '04-01-2017'
+		},
+		{
+			'id': 3,
+			'name': 'Article Three',
+			'body': 'Pish poshhhhhhhhhhhhhhhhhhhhh',
+			'author': 'Dwayne Johnson',
+			'create_date': '04-01-2017' 
+		}
+		
+	]
+
+	return articles
